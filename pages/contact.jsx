b3caffe8b9d427d17f6d397b6e8cf2100d20cb91
Mustafa Tantawy <106565969+motantawi@users.noreply.github.com>
2023-08-06 13:25:06 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -143,18 +143,6 @@ const contact = () => {
               </div>
             </div>
           </div>
-          <div className="flex justify-center py-12">
-            <Link href="#contact">
-              <a>
-                <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
-                  <HiOutlineChevronDoubleUp
-                    className="text-[#5651e5]"
-                    size={30}
-                  />
-                </div>
-              </a>
-            </Link>
-          </div>
         </div>
       </div>
     </>
