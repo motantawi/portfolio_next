18cfd6d680abf7e9a2a9339e90731c42bfd5d88c
Mustafa Tantawy <106565969+motantawi@users.noreply.github.com>
2023-10-15 02:18:13 +0300

    Update skills.jsx

--- pages/skills.jsx
+++ pages/skills.jsx
@@ -136,7 +136,7 @@ const skills = () => {
                   <Image src={Node} width="64px" height="64px" alt="/" />
                 </div>
                 <div className="flex flex-col items-center justify-center">
-                  <h3>PHP</h3>
+                  <h3>Node</h3>
                 </div>
               </div>
             </div>
