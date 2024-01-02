6706a983a1e6a088552c9ad3cca243f615ae7ec0
motantawi <mostafa50.hh@gmail.com>
2024-01-02 23:38:31 +0200

    Changed my Photo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c341e8b2c9552d9"; }
+/******/ 		__webpack_require__.h = function() { return "16c5e1eabdd499d1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/about.jsx
+++ pages/about.jsx
@@ -54,7 +54,7 @@ const about = () => {
               </p>
             </Link>
           </div>
-          <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-3 hover:scale-105 ease-in duration-300">
             <Image src={AboutImg} className="rounded-xl" alt="/" />
           </div>
         </div>
